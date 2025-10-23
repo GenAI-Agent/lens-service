9e509e9ce6f8b3f0bfd3b69f3c9ff6e369218615
chenlunlun33 <hc600hc@gmail.com>
2025-10-23 22:45:42 +0800

    Add server-side exports for Node.js/Next.js API routes

---
+++ dist/server.d.ts
@@ -0,0 +1,13 @@
+/**
+ * Server-side exports for lens-service
+ * Use this file when importing lens-service in Node.js/Next.js API routes
+ */
+export { ContentExtractorService } from './services/ContentExtractorService';
+export { DatabaseService } from './services/DatabaseService';
+export { ManualIndexService } from './services/ManualIndexService';
+export { ConversationService } from './services/ConversationService';
+export { UserService } from './services/UserService';
+export { ConfigService } from './services/ConfigService';
+export { HybridSearchService } from './services/HybridSearchService';
+export { KnowledgeBaseService } from './services/KnowledgeBaseService';
+export { CustomerServiceManager } from './services/CustomerServiceManager';
